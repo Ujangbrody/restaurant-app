54fcf68ed42fcd2a3ae4240f602de07522446e69
Ujangbrody <hssnfalih@gmail.com>
2024-05-18 15:46:20 +0700

    create : like-button-initiator, notification-helper, websocket-initiator

--- src/scripts/utils/like-button-initiator.js
+++ src/scripts/utils/like-button-initiator.js
@@ -1,8 +1,9 @@
 import { async } from "regenerator-runtime";
+import FavoriteRestaurantIdb from "../data/favorite-restaurant-idb";
 import RestaurantSource from "../data/restaurant-source";
 import { createLikeButtonTemplate, createLikedButtonTemplate } from "../views/templates/template-creator";
 
-const likeButtonContainer = {
+const LikeButtonInitiator = {
     async init ({ likeButtonContainer, restaurant }) {
         this._likeButtonContainer = likeButtonContainer;
         this._restaurant = restaurant;
@@ -20,7 +21,30 @@ const likeButtonContainer = {
         }
     },
 
-    // async _isRestaurantExist() {
-    //     const restaurant = await 
-    // },
-}
+    async _isRestaurantExist() {
+        const restaurant = await FavoriteRestaurantIdb.getRestaurant(id);
+        return !!restaurant;
+    },
+
+    _renderLike() {
+        this._likeButtonContainer.innerHTML = createLikeButtonTemplate();
+
+        const likeButton = document.querySelector('#likeButton');
+        likeButton.addEventListener('click', async () => {
+            await FavoriteRestaurantIdb.putRestaurant(this._restaurant);
+            this._renderButton();
+        });
+    },
+
+    _renderLiked() {
+        this._likedButtonContainer.innerHTML = createLikedButtonTemplate();
+
+        const likedButton = document.querySelector('#likedButton');
+        likedButton.addEventListener('click', async () => {
+            await FavoriteRestaurantIdb.deleteRestaurant(this._restaurant);
+            this._renderButton();
+        });
+    },
+};
+
+export default LikeButtonInitiator;
